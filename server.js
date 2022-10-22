fc58b0c172da1307f0798eca6b24e154fa8db067
Hajduk Dušan <62693918+HajdukDusan@users.noreply.github.com>
2022-10-22 20:38:30 +0200

    update

--- server.js
+++ server.js
@@ -9,22 +9,35 @@ Decimal.set({ precision: 124, rounding: 4 })
 const app = express();
 const port = process.env.PORT || 5000;
 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
 
-const provider = new ethers.getDefaultProvider();
+// const provider = new ethers.getDefaultProvider();
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
 app.get('/api/search', async (req, res) => {
 
-    res.send(await GetTransactions(
+    const transactions = await GetExternalTransactions(
         req.query.address,
         req.query.fromBlock,
         req.query.toBlock,
         req.query.pageIndex,
         req.query.pageSize,
         "desc"
-    ));
+    );
+
+    if (shouldBreak(transactions)) res.send([]);
+    console.log("external tx num: ", transactions.length)
+
+    try {
+        res.send(transactions);
+    }
+    catch (error) {
+        console.log(error)
+    }
 });
 
 app.get('/api/erc20/available-amount', async (req, res) => {
@@ -52,14 +65,27 @@ async function GetETHAvailableAmount(address, date) {
     const timestamp = Date.parse(date) / 1000;
     let result = new Decimal('0')
 
-    const latestBlock = await provider.getBlock("latest")
+    const latestBlock = await getLatestBlock()
 
-    const transactions = await GetTransactions(address, 0, latestBlock, 0, 10000, "asc")
-    if (shouldBreak(transactions)) return [];
+    // we need to wait 5 sec between api calls to avoid max rate limit error
+    await delay(5000)
 
-    console.log(transactions.length)
+    const externalTx = await GetExternalTransactions(address, 0, latestBlock, 0, 10000, "asc")
 
-    result = result.plus(accumulateValuesETH(transactions, address, timestamp));
+    await delay(5000)
+
+    const internalTx = await GetInternalTransactions(address, 0, latestBlock, 0, 10000, "asc")
+    if (shouldBreak(externalTx) && shouldBreak(internalTx)) return "0";
+
+    console.log("external tx num: ", externalTx.length)
+    console.log("internal tx num: ", internalTx.length)
+
+    await delay(5000)
+    const isAddressContract = await checkIfAddressIsContract(address);
+
+    result = result.plus(await accumulateValuesETH(externalTx, address, timestamp, isAddressContract));
+    result = result.plus(await accumulateValuesETH(internalTx, address, timestamp, isAddressContract));
+
 
     return result.dividedBy(new Decimal(10).toPower(18))
 }
@@ -68,16 +94,18 @@ async function GetETHAvailableAmount(address, date) {
 async function GetERC20AvailableAmount(address, contractAddress, date) {
 
     const timestamp = Date.parse(date) / 1000;
-    const latestBlock = await provider.getBlock("latest")
+
+    const latestBlock = await getLatestBlock()
+
+    await delay(5000)
 
     const transactions = await GetERC20Transactions(address, contractAddress, 0, latestBlock, 0, 10000, "asc")
+    if (shouldBreak(transactions)) return "0";
 
-    if (shouldBreak(transactions)) return []
+    const decimals = transactions[0].tokenDecimal;
+    console.log("tx num: ", transactions.length)
 
-    const decimals = transactions[0].decimals;
-    console.log(transactions.length)
-
-    let result = accumulateValuesERC20(transactions, address, timestamp);
+    let result = await accumulateValuesERC20(transactions, address, timestamp);
 
     return result.dividedBy(new Decimal(10).toPower(decimals))
 }
@@ -91,7 +119,7 @@ function shouldBreak(transactions) {
     return false
 }
 
-function accumulateValuesETH(transactions, address, timestamp) {
+async function accumulateValuesETH(transactions, address, timestamp, isAddressContract) {
     address = address.toLowerCase()
 
     let result = new Decimal('0');
@@ -100,12 +128,13 @@ function accumulateValuesETH(transactions, address, timestamp) {
 
         if (timestamp < tx.timeStamp) return false;
 
-        if (tx.from === address) {
+        // if tx is a contract it cant pay for gas (it is a internal tx)
+        if (tx.from === address && !isAddressContract) {
             const gasCost = (new Decimal(tx.gasPrice)).times(tx.gasUsed)
             result = result.minus(gasCost)
         }
 
-        // if error value is not sent!
+        // if tx was reverted value is not sent!
         if (tx.isError != 0) return true;
 
         if (tx.to === address) {
@@ -118,6 +147,7 @@ function accumulateValuesETH(transactions, address, timestamp) {
 
         return true;
     });
+
 
     return result;
 }
@@ -141,8 +171,6 @@ function accumulateValuesERC20(transactions, address, timestamp) {
             result = result.minus(tx.value)
         }
 
-        console.log(result)
-
         return true;
     });
 
@@ -163,7 +191,7 @@ async function GetERC20Transactions(address, contractAddress, fromBlock, toBlock
         "&sort=" + order +
         "&apikey=" + process.env.ETHERSCAN_API_KEY
 
-    return await fetchFromEtherscan(url);
+    return (await fetchData(url)).result;
 }
 
 async function GetTransactions(address, fromBlock, toBlock, pageIndex, pageSize, order) {
@@ -179,16 +207,66 @@ async function GetTransactions(address, fromBlock, toBlock, pageIndex, pageSize,
         "&sort=" + order +
         "&apikey=" + process.env.ETHERSCAN_API_KEY
 
-    return await fetchFromEtherscan(url);
+    return (await fetchData(url)).result;
 }
 
-async function fetchFromEtherscan(url) {
+async function GetExternalTransactions(address, fromBlock, toBlock, pageIndex, pageSize, order) {
+    return await GetTransactions("txlist", address, fromBlock, toBlock, pageIndex, pageSize, order)
+}
+
+async function GetInternalTransactions(address, fromBlock, toBlock, pageIndex, pageSize, order) {
+    return await GetTransactions("txlistinternal", address, fromBlock, toBlock, pageIndex, pageSize, order)
+}
+
+async function GetTransactions(action, address, fromBlock, toBlock, pageIndex, pageSize, order) {
+
+    const url = "https://api.etherscan.io/api?" +
+        "module=account" +
+        "&action=" + action +
+        "&address=" + address +
+        "&startblock=" + fromBlock +
+        "&endblock=" + toBlock +
+        "&page=" + pageIndex +
+        "&offset=" + pageSize +
+        "&sort=" + order +
+        "&apikey=" + process.env.ETHERSCAN_API_KEY
+
+    return (await fetchData(url)).result;
+}
+
+async function getAddressCode(address) {
+    const url = "https://api.etherscan.io/api" +
+        "?module=proxy" +
+        "&action=eth_getCode" +
+        "&address=" + address +
+        "&tag=latest" +
+        "&apikey=" + process.env.ETHERSCAN_API_KEY;
+
+    return (await fetchData(url)).result;
+}
+
+async function getLatestBlock() {
+    const url = "https://api.etherscan.io/api?" +
+        "module=proxy" +
+        "&action=eth_blockNumber" +
+        "&apikey=" + process.env.ETHERSCAN_API_KEY;
+
+    return parseInt((await fetchData(url)).result, 16);
+}
+
+async function checkIfAddressIsContract(address) {
+    const code = await getAddressCode(address)
+
+    if (code !== '0x') return true;
+
+    return false;
+}
+
+async function fetchData(url) {
 
     try {
         const data = await fetch(url);
-        const response = await data.json();
-
-        return response.result;
+        return await data.json();
     } catch (error) {
         console.log(error);
     }
